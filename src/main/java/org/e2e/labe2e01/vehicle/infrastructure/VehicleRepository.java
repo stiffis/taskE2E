ac576522ba113d57492586c6da6baf3b41b93a15
stiffis <steve.ildefonso@utec.edu.pe>
2025-04-28 21:42:55 -0500

    fix: delete files

--- src/main/java/org/e2e/labe2e01/vehicle/infrastructure/VehicleRepository.java
+++ src/main/java/org/e2e/labe2e01/vehicle/infrastructure/VehicleRepository.java
@@ -5,6 +5,4 @@ import org.e2e.labe2e01.vehicle.domain.Vehicle;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface VehicleRepository extends JpaRepository<Vehicle,Long> {
-	List<Vehicle> findByBrandInAndFabricationYearGreaterThanEqualAndFabricationYearLessThanEqual(
-			List<String> brands, int year);
 }
